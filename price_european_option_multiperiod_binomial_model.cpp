31011d667fce0cd511daaa7f8beb17e0854334a4
Casey Conger <c.conger@trestlegroup.com>
2017-01-22 21:53:43 -0500

    test

--- price_european_option_multiperiod_binomial_model.cpp
+++ price_european_option_multiperiod_binomial_model.cpp
@@ -3,173 +3,8 @@
 
 using namespace std;
 
-// class node
-// {
-// private:
-// 	int _time;
-// 	int _height;
-// 	double _value;
-// 	node * _up;
-// 	node * _down;
-// public:
-// 	node(){};
-// 	node(int time,
-// 		 int height);
-// 	node(int time,
-// 		 int height,
-// 		 double value);
 
-// 	int time();
-// 	int height();
-// 	double value();
-// };
-
-// int node::time() { return _time; }
-// int node::height() { return _height; }
-// double node::value() { return _option_value; }
-
-// node::node(int time,
-// 		   int height) {
-
-// 	_time = time;
-// 	_height = height;
-// }
-
-// node::node(int time,
-// 		   int height,
-// 		   double value) {
-
-// 	_time = time;
-// 	_height = height;
-// 	_value = value;
-// }
-
-// class tree
-// {
-// private:
-// 	node _up;
-// 	node _down;
-// 	node _initial;
-// public:
-// 	tree(){};
-// 	tree(node up_node,
-// 		 node down_node);
-
-// 	node up();
-// 	node down();
-// 	void set_up_node(node up_node);
-// 	void set_down_node(node down_node);
-// };
-
-// tree::tree(node up_node,
-// 		   node down_node) {
-
-// 	_up = up_node;
-// 	_down = down_node;
-// }
-
-// struct node
-// {
-// 	int value;
-// 	node *up;
-// 	node *down;
-// };
-
-// class tree
-// {
-// public:
-// 	tree();
-// 	~tree();
-
-// 	void insert(int value);
-// 	node *search(int value);
-// 	void destroy_tree();
-
-// private:
-// 	void destroy_tree(node *leaf);
-// 	void insert(int value, node *leaf);
-// 	node *search(int value, node *leaf);
-
-// 	node *root;
-// };
-
-// tree::tree(){
-// 	root = NULL;
-// }
-
-// tree::~tree() {
-// 	destroy_tree();
-// }
-
-// void tree::destroy_tree(node * leaf) {
-// 	if (leaf != NULL){
-// 		destroy_tree(leaf->down);
-// 		destroy_tree(leaf->up);
-// 		delete leaf;
-// 	}
-// }
-
-// void tree::insert(int value, node *leaf) {
-// 	if (value < leaf->value){
-// 		if (leaf->down != NULL){
-// 			insert(value, leaf->down);
-// 		}
-// 		else {
-// 			leaf->down = new node;
-// 			leaf->down->value = value;
-// 			leaf->down->down = NULL;
-// 			leaf->down->up = NULL;
-// 		}
-// 	}
-// 	else if (value >= leaf->value){
-// 		if (leaf->up != NULL){
-// 			insert(value, leaf->up);
-// 		}
-// 		else {
-// 			leaf->up = new node;
-// 			leaf->up->value = value;
-// 			leaf->up->down = NULL;
-// 			leaf->up->up = NULL;
-// 		}
-// 	}
-// }
-
-// node *tree::search(int value, node *leaf) {
-// 	if (leaf != NULL) {
-// 		if (value == leaf->value) {
-// 			return leaf;
-// 		}
-// 		if (value < leaf->value) {
-// 			return search(value, leaf->down);
-// 		}
-// 		else {
-// 			return search(value, leaf->up);
-// 		}
-// 	}
-// 	else return NULL;
-// }
-
-// void tree::insert(int value) {
-// 	if (root != NULL) {
-// 		insert(value, root);
-// 	}
-// 	else {
-// 		root = new node;
-// 		root->value = value;
-// 		root->down = NULL;
-// 		root->up = NULL;
-// 	}
-// }
-
-// node *tree::search(int value) {
-// 	return search(value, root);
-// }
-
-// void tree::destroy_tree() {
-// 	destroy_tree(root);
-// }
-
-
+// Node struct
 struct node
 {
 	double value;
@@ -179,16 +14,19 @@ struct node
 	node * down;
 };
 
+
+// Basic tree class
 class Tree
 {
-private:
+protected:
 	node ** _treeArray;
 
-
 public:
+	// Constructors & Destructors
 	Tree();
 	Tree(int numTimeSteps);
 	~Tree();
+
 
 	double value(int time, int height);
 	void set(int time, int height, double value);
@@ -202,7 +40,6 @@ Tree::Tree(int numTimeSteps) {
 		_treeArray[i] = new node[i + 2];
 	}
 }
-
 
 Tree::~Tree() {
 	delete _treeArray;
@@ -220,10 +57,7 @@ void Tree::set(int time, int height, double value) {
 }
 
 
-// class SecurityTree : public tree
-// {
 
-// };
 
 
 
