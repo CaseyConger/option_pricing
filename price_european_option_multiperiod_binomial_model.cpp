23cf61f8b863a810802fb6b48b1787b69b75bb10
Casey Conger <c.conger@trestlegroup.com>
2017-01-22 22:53:56 -0500

    Finished SecurityTree class.

--- price_european_option_multiperiod_binomial_model.cpp
+++ price_european_option_multiperiod_binomial_model.cpp
@@ -20,6 +20,7 @@ class Tree
 {
 protected:
 	node ** _treeArray;
+	int _numTimeSteps;
 
 public:
 	// Constructors & Destructors
@@ -31,13 +32,25 @@ public:
 	double value(int time, int height);
 	void set(int time, int height, double value);
 
+	void print();
+
 };
 
 Tree::Tree(int numTimeSteps) {
+	_numTimeSteps = numTimeSteps;
 	_treeArray = new node*[numTimeSteps + 1];
 	_treeArray[0] = new node[1];
-	for (int i = 1; i <= numTimeSteps; ++i) {
+	for (int i = 1; i <= _numTimeSteps; ++i) {
 		_treeArray[i] = new node[i + 2];
+	}
+
+	for (int i = 0; i < _numTimeSteps + 1; ++i) {
+		for (int j = 0; j <= i; ++j){
+			_treeArray[i][j].time = i;
+			_treeArray[i][j].height = j;
+			_treeArray[i][j].down = &_treeArray[i + 1][j];
+			_treeArray[i][j].up = &_treeArray[i + 1][j + 1];
+		}
 	}
 }
 
@@ -56,9 +69,45 @@ void Tree::set(int time, int height, double value) {
 	_treeArray[time][height].height = height;
 }
 
+void Tree::print() {
+	for (int i = 0; i < _numTimeSteps + 1; ++i) {
+		for (int j = 0; j <= i; ++j) {
+			cout << "[" << i << "][" << j << "] = ";
+			cout << _treeArray[i][j].value << endl;
+		}
+	}
+}
 
 
+// Security Tree
+class SecurityTree : public Tree {
+private:
+	double _up;
+	double _down;
+	double _S_0;
+public:
+	SecurityTree(double upFactor,
+				 double S_0,
+				 int numTimeSteps);
 
+};
+
+SecurityTree::SecurityTree(double upFactor,
+						   double S_0,
+						   int numTimeSteps)
+				: Tree(numTimeSteps) {
+	_up = upFactor;
+	_down = 1.0/_up;
+	_S_0 = S_0;
+
+	for (int i = 0; i < _numTimeSteps + 1; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+			_treeArray[i][j].value = _S_0 * pow(_up, j) * pow(_down, i - j);
+		}
+	}
+}
 
 
 int main(int argc, char* argv[]) {
@@ -74,7 +123,8 @@ int main(int argc, char* argv[]) {
 	// cout << "--------" << endl;
 
 
-
+	SecurityTree first_tree(2.0, 100., 15);
+	first_tree.print();
 
 
 	return 0;
